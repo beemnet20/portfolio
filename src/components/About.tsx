ffffbcd56c4213e3518982848fbc442102fa91d7
beemnet20 <61217732+beemnet20@users.noreply.github.com>
2024-07-07 17:53:35 -0700

    update modules

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const About: React.FC = () => {
   const skills = [
@@ -25,32 +26,48 @@ const About: React.FC = () => {
       style={{ scrollMarginTop: '100px' }}
       className='flex m-8 flex-col h-screen'
     >
-      <img
-        className='w-40 h-40 rounded-full mb-4'
-        src='/portfolio/me.jpg'
-        alt='Profile Picture'
-      />
-      <h1 className='text-2xl font-bold mb-2'>About me</h1>
-      <p className='text-lg'>
-        I'm Beemnet (Bee) Workeneh, a full stack developer based in Seattle, WA.
-        My educational background is in Mechanical Engineering. My capstone
-        project was the first time that I was exposed to coding and I fell in
-        love with it. I have been coding ever since. I am passionate about
-        creating beautiful and functional applications. This website contains
-        some of projects that I have worked that highlight a subset of my
-        skills.
-      </p>
+      <div className='grid grid-cols-4 gap-4'>
+        <Image
+          width={250}
+          height={250}
+          className='rounded-full flex-col '
+          src='/portfolio/me.jpg'
+          alt='Profile Picture'
+        />
+
+        <section className='col-span-3'>
+          <h1 className='text-2xl font-bold mb-2'>About me</h1>
+          <p className='text-lg'>
+            I'm Beemnet (Bee) Workeneh, a full stack developer based in Seattle,
+            WA. My educational background is in Mechanical Engineering. My
+            capstone project was the first time that I was exposed to coding and
+            I fell in love with it. I have been coding ever since. I am
+            passionate about creating beautiful and functional applications.
+            This website contains some of projects that I have worked that
+            highlight a subset of my skills.
+          </p>
+        </section>
+      </div>
+
       <br />
       <h2 className='text-2xl'>Skills</h2>
-      
+
       <div className='mt-2 grid grid-cols-4 gap-4'>
         {skills.map((skill) => (
-          <div key={skill.skill} className='flex flex-col rounded-lg items-center justify-center bg-white'>
-            <img className='w-16 h-16 mt-2' src={skill.icon} alt={skill.skill} />
+          <div
+            key={skill.skill}
+            className='flex flex-col rounded-lg items-center justify-center bg-white'
+          >
+            <Image
+              className='mt-2'
+              width={50}
+              height={50}
+              src={skill.icon}
+              alt={skill.skill}
+            />
             <p className='text-black m-1'>{skill.skill}</p>
           </div>
         ))}
-
       </div>
       <span>and more...</span>
     </div>
