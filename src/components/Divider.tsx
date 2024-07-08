d19d7fbe5b6a138fa436c30e33ec9f3853faff08
beemnet20 <61217732+beemnet20@users.noreply.github.com>
2024-07-07 18:02:57 -0700

    add divider

---
+++ src/components/Divider.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Divider: React.FC = () => {
+    return (
+        <hr className="h-0.5 border-t-0 bg-yellow-900" />
+    );
+}
+
+export default Divider;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import About from "@/components/About";
+import Divider from "@/components/Divider";
 
 const inter = Inter({ subsets: ["latin"] });
 console.log(inter)
@@ -12,7 +13,9 @@ export default function Home() {
     >
       <Navbar />
       <Hero />
+      <Divider />
       <About />
+      <Divider />
     </main>
   );
 }
