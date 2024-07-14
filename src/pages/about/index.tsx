597ebc2810867286bd0c7501ab265c0bc60d1e17
beemnet20 <61217732+beemnet20@users.noreply.github.com>
2024-07-14 10:09:40 -0700

    add external links

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import PageTitle from '../../components/PageTitle';
+import { link } from 'fs';
 
-const SkillCard = (skill: string, icon: string) => {
+const SkillCard = (skill: string, icon: string, description: string, link: string) => {
   return (
     <div key={skill} className=' rounded-lg  bg-gray-100'>
       <div className='flex flex-row justify-between'>
@@ -20,12 +21,20 @@ const SkillCard = (skill: string, icon: string) => {
             </span>
             <br />
             <span className='text-black text-sm  mx-0 inline-block'>
-              description
+              {description}
             </span>
           </div>
         </div>
 
-        <button className='text-gray-500 text-4xl mx-4 font-bold my-auto'>⋮</button>
+        <a href={link} target='_blank' className='flex p-4'>
+          <Image
+            className=''
+            width={25}
+            height={25}
+            src='/portfolio/external-link.svg'
+            alt=''
+          />
+        </a>
       </div>
     </div>
   );
@@ -36,22 +45,22 @@ const About: React.FC = () => {
   const [size, setSize] = useState<number>(200);
 
   const skills = [
-    { skill: 'React', icon: '/portfolio/logos/react.svg' },
-    { skill: 'Next.js', icon: '/portfolio/logos/nextjs.svg' },
-    { skill: 'Node.js', icon: '/portfolio/logos/nodejs.svg' },
-    { skill: 'PostgreSQL', icon: '/portfolio/logos/postgresql.svg' },
-    { skill: 'JavaScript', icon: '/portfolio/logos/javascript.svg' },
-    { skill: 'TypeScript', icon: '/portfolio/logos/typescript.svg' },
-    { skill: 'HTML', icon: '/portfolio/logos/html.svg' },
-    { skill: 'CSS', icon: '/portfolio/logos/css.svg' },
-    { skill: 'Python', icon: '/portfolio/logos/python.svg' },
-    { skill: 'Flask', icon: '/portfolio/logos/flask.svg' },
-    { skill: 'Git', icon: '/portfolio/logos/git.svg' },
-    { skill: 'R', icon: '/portfolio/logos/r.svg' },
-    { skill: 'Docker', icon: '/portfolio/logos/docker.svg' },
-    { skill: 'Kubernetes', icon: '/portfolio/logos/kubernetes.svg' },
-    { skill: 'linux', icon: '/portfolio/logos/linux.svg' },
-    { skill: 'Figma', icon: '/portfolio/logos/figma.svg' },
+    { skill: 'React', description: 'UI Library', icon: '/portfolio/logos/react.svg', link: 'https://react.dev/' },
+    { skill: 'Next.js', description: 'React Framework', icon: '/portfolio/logos/nextjs.svg', link: 'https://nextjs.org/' },
+    { skill: 'Node.js', description: 'JS Runtime', icon: '/portfolio/logos/nodejs.svg', link: 'https://nodejs.org/' },
+    { skill: 'PostgreSQL', description: 'SQL Database', icon: '/portfolio/logos/postgresql.svg', link: 'https://www.postgresql.org/'},
+    { skill: 'JavaScript', description: 'Programming Language', icon: '/portfolio/logos/javascript.svg', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript'},
+    { skill: 'TypeScript', description: 'JS Superset', icon: '/portfolio/logos/typescript.svg', link: 'https://www.typescriptlang.org/'},
+    { skill: 'HTML', description: 'Markup Language', icon: '/portfolio/logos/html.svg', link: 'https://developer.mozilla.org/en-US/docs/Web/HTML'},
+    { skill: 'CSS', description: 'Style Sheets', icon: '/portfolio/logos/css.svg', link: 'https://developer.mozilla.org/en-US/docs/Web/CSS'},
+    { skill: 'Python', description: 'Programming Language', icon: '/portfolio/logos/python.svg', link: 'https://www.python.org/' },
+    { skill: 'Flask', description: 'Web Framework', icon: '/portfolio/logos/flask.svg', link: 'https://flask.palletsprojects.com/' },
+    { skill: 'Git', description: 'Version Control', icon: '/portfolio/logos/git.svg', link: 'https://git-scm.com/' },
+    { skill: 'R', description: 'Stats  Language', icon: '/portfolio/logos/r.svg', link: 'https://www.r-project.org/' },
+    { skill: 'Docker', description: 'Containerization', icon: '/portfolio/logos/docker.svg', link: 'https://www.docker.com/'},
+    { skill: 'Kubernetes', description: 'Orchestration', icon: '/portfolio/logos/kubernetes.svg', link: 'https://kubernetes.io/'},
+    { skill: 'linux', description: 'OS Kernel', icon: '/portfolio/logos/linux.svg', link: 'https://www.linux.org/'},
+    { skill: 'Figma', description: 'Design Tool', icon: '/portfolio/logos/figma.svg', link: 'https://www.figma.com/'},
   ];
   return (
     <div className='flex m-8 flex-col h-full'>
@@ -86,7 +95,7 @@ const About: React.FC = () => {
           <br />
           <h2 className='text-lg'>I have experience with </h2>
           <div className='mt-2 grid sm:grid-cols-2 lg:grid-cols-3 gap-4 w-half'>
-            {skills.map((skill) => SkillCard(skill.skill, skill.icon))}
+            {skills.map((skill) => SkillCard(skill.skill, skill.icon, skill.description, skill.link))}
           </div>
           <span>and more...</span>
         </section>
