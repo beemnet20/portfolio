374b9d53893d0528a17ed628db61ed84deb6c48d
beemnet20 <61217732+beemnet20@users.noreply.github.com>
2024-07-14 17:53:06 -0700

    started projects

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Divider: React.FC = () => {
     return (
-        <hr className="h-0.5 border-t-0 bg-yellow-900" />
+        <hr style={{"height":"1px"}} className="border-t-0 bg-yellow-900" />
     );
 }
 
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,12 +1,22 @@
+'use client'
 import React from 'react';
 import PageTitle from '../../components/PageTitle';
+import useSWR from 'swr';
+import ProjectCard, {Project} from '@/components/ProjectCard';
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
 const Projects: React.FC = () => {
+  const { data, error } = useSWR(
+    `/portfolio/data/projects.json`,
+    fetcher,
+  );
+  if (error) return 'Failed to load';
   return (
-    <div
-      className='flex m-8 flex-col h-full'
-    >
-        <PageTitle title='Projects' id='projects'/>
+    <div className='flex m-8 flex-col h-full'>
+      <PageTitle title='Projects' id='projects' />
+      {!data && <div>Loading...</div>
+      }
+      {data && data.map((project: Project) => <ProjectCard key={project.title} project={project}/> )}
     </div>
   );
 };
