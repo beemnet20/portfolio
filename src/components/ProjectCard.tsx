849a97b9105208652928779d4cca5526fd732c31
beemnet20 <61217732+beemnet20@users.noreply.github.com>
2024-07-15 21:16:42 -0700

    update projects

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -8,34 +8,71 @@ export type Project = {
   description: string;
   tags: string[];
   image: string;
-  actions: { url: string; title: string ; icon?: string }[];
+  actions: { url: string; title: string; icon?: string }[];
 };
 
 export default function ProjectCard({ project }: { project: Project }) {
   return (
-    <div className='max-w-sm rounded overflow-hidden shadow-lg bg-black'>
-      <Image src={project.image} alt={project.title} width={500} height={500} />
+    <div className='max-w-sm rounded overflow-hidden shadow-yellow-900 shadow-lg bg-black flex flex-col justify-between'>
+      <Image src={project.image} alt={project.title} width={500} height={250} />
       <div className='px-6 py-4'>
         <div className='font-bold text-xl mb-2'>{project.title}</div>
-        <p className='text-gray-200 text-base'>{project.description}</p>
+        <p className=''>{project.description}</p>
       </div>
-      <Divider />
       <div className='px-6 pt-2 pb-2'>
         {project.tags.map((tag) => (
-          <span key={tag} className='inline-block bg-gray-200 rounded-full px-3 py-1 text-sm  text-gray-700 mr-2 mb-2'>
+          <span
+            key={tag}
+            className='inline-block border rounded-full px-3 py-1 text-sm  text-base mr-2 mb-2'
+          >
             {tag}
           </span>
         ))}
       </div>
-
-      <Divider />
-      <div className='content-center p-2'>
-        {project.actions.map((action) => 
-            <div key={action.title} className = {`${action.title === "View source" ? "float-left":"float-right"} flex`} >
-            {action.icon && <a target="_blank" href={action.url} className='bg-white p-2 mx-2 rounded-full inline-block'> <Image src={action.icon} alt={action.title} width={25} height={25} /></a>}
-            {action.title != "View source" && <a target='_blank' className='mx-2 bg-yellow-500 hover:bg-yellow-700 text-black font-bold p-2 rounded-full inline-block '>{action.title}{' '}<Image className='inline' src="/portfolio/external-link.svg" alt={action.title} width={25} height={25} /></a>}
+      <div className='actions'>
+        <Divider />
+        <div className='content-center p-2'>
+          {project.actions.map((action) => (
+            <div
+              key={action.title}
+              className={`${
+                action.title === 'View source' ? 'float-left' : 'float-right'
+              } flex`}
+            >
+              {action.icon && (
+                <a
+                  target='_blank'
+                  href={action.url}
+                  className='bg-white p-2 mx-2 rounded-full inline-block'
+                >
+                  {' '}
+                  <Image
+                    src={action.icon}
+                    alt={action.title}
+                    width={25}
+                    height={25}
+                  />
+                </a>
+              )}
+              {action.title != 'View source' && (
+                <a
+                  target='_blank'
+                  href={action.url}
+                  className='mx-2 bg-yellow-500 hover:bg-yellow-700 text-black font-bold p-2 rounded-full inline-block '
+                >
+                  {action.title}{' '}
+                  <Image
+                    className='inline'
+                    src='/portfolio/external-link.svg'
+                    alt={action.title}
+                    width={25}
+                    height={25}
+                  />
+                </a>
+              )}
             </div>
-        )}
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,22 +1,23 @@
-'use client'
+'use client';
 import React from 'react';
 import PageTitle from '../../components/PageTitle';
 import useSWR from 'swr';
-import ProjectCard, {Project} from '@/components/ProjectCard';
-const fetcher = (url: string) => fetch(url).then((r) => r.json())
+import ProjectCard, { Project } from '@/components/ProjectCard';
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 const Projects: React.FC = () => {
-  const { data, error } = useSWR(
-    `/portfolio/data/projects.json`,
-    fetcher,
-  );
+  const { data, error } = useSWR(`/portfolio/data/projects.json`, fetcher);
   if (error) return 'Failed to load';
   return (
     <div className='flex m-8 flex-col h-full'>
       <PageTitle title='Projects' id='projects' />
-      {!data && <div>Loading...</div>
-      }
-      {data && data.map((project: Project) => <ProjectCard key={project.title} project={project}/> )}
+      {!data && <div>Loading...</div>}
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {data &&
+          data.map((project: Project) => (
+            <ProjectCard key={project.title} project={project} />
+          ))}
+      </div>
     </div>
   );
 };
