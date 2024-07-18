f44908fa9a1802ceaddf2f79a8c3759c9de535b5
beemnet20 <61217732+beemnet20@users.noreply.github.com>
2024-07-17 21:13:22 -0700

    update projects

---
+++ src/components/FeaturedImageGallery.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Image from 'next/image';
+// Define the type for a single image object
+interface ImageData {
+  url: string;
+  caption?: string;
+}
+
+// Define the props for the FeaturedImageGallery component
+interface FeaturedImageGalleryProps {
+  data: ImageData[];
+}
+
+// The FeaturedImageGallery component
+const FeaturedImageGallery: React.FC<FeaturedImageGalleryProps> = ({
+  data,
+}) => {
+  // State to keep track of the active image URL
+  const [active, setActive] = React.useState(data[0]?.url || '');
+  const [activeCaption, setActiveCaption] = React.useState(data[0]?.caption || '');
+  return (
+    <div className='grid gap-4 m-6 mx-auto md:w-2/3'>
+      <h2 className='text-2xl text-center font-bold'>Images</h2>
+      <div className='flex justify-between'>
+        {data.map((item, index) => (
+          <div key={index} onClick={() => {setActive(item.url); setActiveCaption(item.caption ?? '')}}>
+            <Image
+              height={100}
+              width={100}
+              src={item.url}
+              className='h-20 max-w-full cursor-pointer object-cover object-center'
+              alt='gallery-image'
+            />
+          </div>
+        ))}
+      </div>
+      <div>
+        <p className='text-center m-2'>{activeCaption}</p>
+        <Image
+          className='h-auto w-full max-w-full  object-cover object-center md:h-[500px] '
+          src={active}
+          alt=''
+          height={400}
+          width={400}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedImageGallery;
--- src/pages/projects/load-a-dose/index.tsx
+++ src/pages/projects/load-a-dose/index.tsx
@@ -1,72 +1,156 @@
 import React from 'react';
 import Image from 'next/image';
 import PageTitle from '@/components/PageTitle';
+import FeaturedImageGallery from '@/components/FeaturedImageGallery';
+
+const data = [
+  {
+    url: '/portfolio/data/load-a-dose/load-a-dose-prototypes-and-nfc-reader.jpg',
+    caption: 'Prototypes and NFC reader',
+  },
+  {
+    url: '/portfolio/data/load-a-dose/load-a-dose-device-inside.jpg',
+    caption: 'Inside the Load-A-Dose device',
+  },
+  {
+    url: '/portfolio/data/load-a-dose/load-a-dose-arduino-mega-custom-shield.jpg',
+    caption: 'Arduino Mega with custom shield',
+  },
+  {
+    url: '/portfolio/data/load-a-dose/load-a-dose-wiring-diagram.jpg',
+    caption: 'Wiring diagram for Load-A-Dose',
+  },
+  {
+    url: '/portfolio/data/load-a-dose/load-a-dose-nfc-reader-wiring-diagram.jpg',
+    caption: 'Wiring diagram for NFC reader that reads insulin vials',
+  },
+];
 
 const LoadADose: React.FC<{}> = () => {
   return (
     <div className='flex m-8 flex-col h-full'>
       <PageTitle title='LOAD-A-DOSE' id='load-a-dose' />
-      <div className='grid  md:grid-cols-4 lg:grid-cols-10 gap-3'>
-        <div className='lg:col-span-2 md:col-span-1'>
-          <div className=''>
-            <Image
-              src='/portfolio/data/load-a-dose-device.jpg'
-              alt='Load-A-Dose'
-              width={449}
-              height={540}
-            />
-          </div>
-        </div>
+      <div className=' md:w-2/3 flex mx-auto'>
+        <div className=''></div>
 
-        <div className='lg:col-span-8 md:col-span-3'>
-          <p className='text-lg'>
-            Load-A-Dose is an electro-mechanical medical device that takes user
-            input to measure and draw insulin from a vial into a syringe. It has
-            an LCD screen to display the reading and a speaker that reads aloud
-            the information displayed. The target user group for the device is
-            diabetic people that are blind or have low visual acuity. It has
-            large tactile buttons that are designed with the key demographic in
-            mind. There are many insulin devices in the market, what makes
-            Load-A-Dose stand out is it&apos;s affordability. Syringe and vial are
-            the cheapest form of insulin administration compared to syringe pens
-            and closed loop insulin pumps. However for a person with low visual
-            acuity the small markers on the already small syringes makes it
-            difficult to self administer insulin accurately, leading to further
-            health complications. <br />
+        <div className=''>
+          <div className='flex flex-col lg:flex-row'>
+            <div className='m-4'>
+              <Image
+                src='/portfolio/data/load-a-dose/load-a-dose-device.jpg'
+                alt='Load-A-Dose'
+                className=''
+                width={1800}
+                height={1800}
+              />
+            </div>
+            <p className='text-lg inline m-2'>
+              Load-A-Dose is an electro-mechanical medical device that takes
+              user input to measure and draw insulin from a vial into a syringe.
+              It has an LCD screen to display the reading and a speaker that
+              reads aloud the information displayed. The target user group for
+              the device is diabetic people that are blind or have low visual
+              acuity. It has large tactile buttons that are designed with the
+              key demographic in mind. There are many insulin devices in the
+              market, what makes Load-A-Dose stand out is it&apos;s
+              affordability. Syringe and vial are the cheapest form of insulin
+              administration compared to syringe pens and closed loop insulin
+              pumps. However for a person with low visual acuity the small
+              markers on the already small syringes makes it difficult to self
+              administer insulin accurately, leading to further health
+              complications.
+            </p>
+          </div>
+          <p className='m-2'>
             Load-A-Dose was designed as part of Engineering in Health senior
             project at the University of Washington. My role was to design and
             prototype the hardware and firmware. I also designed some of the
             shrouding and the attachments to the linear actuator.
-            <br />I also designed and built a prototype of a Near Field
-            Communications reader for insulin vials. As there are many types of
-            insulin it is crucial that a person with low visual acuity is able
-            to administer the correct type. Although there are products like
-            this one in the market (designed mainly for pill bottles) ours is
-            designed to be integrated with the Load-A-Dose device itself.
+          </p>
+          <p className='m-2'>
+            I also designed and built a prototype of a Near Field Communications
+            reader for insulin vials. As there are many types of insulin it is
+            crucial that a person with low visual acuity is able to administer
+            the correct type. Although there are products like this one in the
+            market (designed mainly for pill bottles) ours is designed to be
+            integrated with the Load-A-Dose device itself.
           </p>
         </div>
       </div>
-      <h2 className='text-2xl font-bold my-2'>How it works</h2>
-      <div className='grid md:grid-cols-5 gap-4'>
-        <div className='md:col-span-1'>
-          <video width={300}  controls controlsList='nodownload'>
-            <source src='/portfolio/data/load-a-dose-video.mp4' />
+      <FeaturedImageGallery data={data} />
+      <h2 className='text-2xl font-bold my-2 mx-auto'>How it works</h2>
+      <div className='flex justify-center flex-col sm:flex-row'>
+        <div className='mx-2'>
+          <video width={300} controls controlsList='nodownload'>
+            <source src='/portfolio/data/load-a-dose/load-a-dose-video.mp4' />
           </video>
         </div>
-        <div className='md:col-span-2 mx-4'>
-          <h2 className='text-xl font-bold my-2'>Steps</h2>
-          <ol className='text-xl list-decimal' type='1'>
-            <li>Load syringe and insulin vial into device</li>
-            <li>Start device by pushing start button</li>
-            <li>Enter x amount of units to be drawn (default 10)</li>
-            <li>Device draws x amount of air into syringe</li>
-            <li>Vial is lowered and air is injected into it</li>
-            <li>The device draws x amount insulin from the vial</li>
-            <li>The vial is raised</li>
-            <li>The syringe is removed</li>
+        <div className='mx-2'>
+          <ol
+            className='items-center w-full space-y-4  rtl:space-x-reverse'
+            type='1'
+          >
+            <li className='flex items-center space-x-2.5 rtl:space-x-reverse'>
+              <span className='flex items-center justify-center w-8 h-8 border border-yellow-600 rounded-full shrink-0'>
+                1
+              </span>
+              <span>Load syringe and vial into device</span>
+            </li>
+            <li className='flex items-center space-x-2.5 rtl:space-x-reverse'>
+              <span className='flex items-center justify-center w-8 h-8 border border-yellow-600 rounded-full shrink-0'>
+                2
+              </span>
+              <span>Press start button</span>
+            </li>
+            <li className='flex items-center space-x-2.5 rtl:space-x-reverse'>
+              <span className='flex items-center justify-center w-8 h-8 border border-yellow-600 rounded-full shrink-0'>
+                3
+              </span>
+              <span>
+                Enter <span className='text-yellow-500'>x</span> amount of units
+                to be drawn
+              </span>
+            </li>
+            <li className='flex items-center space-x-2.5 rtl:space-x-reverse'>
+              <span className='flex items-center justify-center w-8 h-8 border border-yellow-600 rounded-full shrink-0'>
+                4
+              </span>
+              <span>
+                <span className='text-yellow-500'>x</span> amount of air drawn
+                into syringe
+              </span>
+            </li>
+            <li className='flex items-center space-x-2.5 rtl:space-x-reverse'>
+              <span className='flex items-center justify-center w-8 h-8 border border-yellow-600 rounded-full shrink-0'>
+                5
+              </span>
+              <span>Vial lowered and injected with air</span>
+            </li>
+            <li className='flex items-center space-x-2.5 rtl:space-x-reverse'>
+              <span className='flex items-center justify-center w-8 h-8 border border-yellow-600 rounded-full shrink-0'>
+                6
+              </span>
+              <span>
+                <span className='text-yellow-500'>x</span> amount of insulin
+                drawn from the vial
+              </span>
+            </li>
+            <li className='flex items-center space-x-2.5 rtl:space-x-reverse'>
+              <span className='flex items-center justify-center w-8 h-8 border border-yellow-600 rounded-full shrink-0'>
+                7
+              </span>
+              <span>The vial is raised</span>
+            </li>
+            <li className='flex items-center space-x-2.5 rtl:space-x-reverse'>
+              <span className='flex items-center justify-center w-8 h-8 border border-yellow-600 rounded-full shrink-0'>
+                8
+              </span>
+              <span>The syringe is removed</span>
+            </li>
           </ol>
         </div>
       </div>
+
     </div>
   );
 };
